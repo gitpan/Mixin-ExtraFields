c8a3447ada33bd386d0ae0576203dd03dda265ca
Ricardo SIGNES <rjbs@cpan.org>
2006-12-09 01:53:23 +0000

    Import of RJBS/Mixin-ExtraFields-0.001 from CPAN.
    gitpan-cpan-distribution: Mixin-ExtraFields
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         RJBS/Mixin-ExtraFields-0.001.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

---
+++ lib/Mixin/ExtraFields/Driver/HashGuts.pm
@@ -0,0 +1,132 @@
+
+use strict;
+use warnings;
+
+package Mixin::ExtraFields::Driver::HashGuts;
+use base qw(Mixin::ExtraFields::Driver);
+
+=head1 NAME
+
+Mixin::ExtraFields::Driver::HashGuts - store extras in a hashy object's guts
+
+=head1 VERSION
+
+version 0.001
+
+ $Id: HashGuts.pm 26336 2006-12-09 01:46:51Z rjbs $
+
+=cut
+
+our $VERSION = '0.001';
+
+=head1 SYNOPSIS
+
+  package Your::HashBased::Class;
+
+  use Mixin::ExtraFields -fields => { driver => 'HashGuts' };
+
+=head1 DESCRIPTION
+
+This driver class implements an extremely simple storage mechanism: extras are
+stored on the object on which the mixed-in methods are called.  By default,
+they are stored in the key C<__extras>, but this can be changed by providing a
+C<hash_key> argument to the driver configuration, like so:
+
+  use Mixin::ExtraFields -fields => {
+    driver => { class => 'HashGuts', hash_key => "\0Something\0Wicked\0" }
+  };
+
+=head1 METHODS
+
+In addition to the methods required by Mixin::ExtraFields::Driver, the
+following methods are provided:
+
+=head2 hash_key
+
+  my $key = $driver->hash_key;
+
+This method returns the key where the driver will store its extras.
+
+=cut
+
+sub from_args {
+  my ($class, $arg) = @_;
+
+  my $self = {
+    hash_key => $arg->{hash_key} || '__extras',
+  };
+
+  bless $self => $class;
+}
+
+sub hash_key {
+  my ($self) = @_;
+  return $self->{hash_key};
+}
+
+sub exists_extra {
+  my ($self, $object, $id, $name) = @_;
+
+  return exists $object->{ $self->hash_key }{ $name };
+}
+
+sub get_extra {
+  my ($self, $object, $id, $name) = @_;
+
+  # avoid autovivifying entries on get.
+  return unless $self->exists_extra($object, $id, $name);
+  return $object->{ $self->hash_key }{ $name };
+}
+
+sub get_detailed_extra {
+  my ($self, $object, $id, $name) = @_;
+
+  # avoid autovivifying entries on get.
+  return unless $self->exists_extra($object, $id, $name);
+  return { value => $object->{ $self->hash_key }{ $name } };
+}
+
+sub get_all_detailed_extra {
+  my ($self, $object, $id) = @_;
+
+  return unless my $stash = $object->{ $self->hash_key };
+  my @all_detailed = map { $_ => { value => $stash->{$_} } } keys %$stash;
+}
+
+sub get_all_extra {
+  my ($self, $object, $id) = @_;
+
+  return unless my $hash_ref = $object->{ $self->{hash_key} };
+  return %$hash_ref;
+}
+
+sub set_extra {
+  my ($self, $object, $id, $name, $value) = @_;
+
+  return $object->{ $self->hash_key }{ $name } = $value;
+}
+
+sub delete_extra {
+  my ($self, $object, $id, $name) = @_;
+
+  delete $object->{ $self->hash_key }{ $name };
+}
+
+sub delete_all_extra {
+  my ($self, $object, $id) = @_;
+  $object->{ $self->hash_key } = undef;
+}
+
+=head1 AUTHOR
+
+This code was written by Ricardo SIGNES.  His code in 2006 was sponsored by
+Listbox.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2006, Ricardo SIGNES.  This code is free software, and is
+available under the same terms as perl itself.
+
+=cut
+
+1;
---
+++ t/basic.t
@@ -0,0 +1,66 @@
+use strict;
+use warnings;
+
+use Test::More tests => 19;
+
+BEGIN { require_ok('Mixin::ExtraFields'); }
+
+use lib 't/lib';
+
+my $test_class;
+
+BEGIN {
+  $test_class = 'Object::HasExtraFields';
+  use_ok($test_class);
+}
+
+my $object = $test_class->new;
+
+isa_ok($object, $test_class);
+
+can_ok(
+  $object,
+  map { "$_\_extra" } qw(get get_all set exists delete delete_all),
+);
+
+ok( ! $object->exists_extra('datum'), "there exists no extra 'datum' yet");
+is($object->get_extra('datum'), undef, "extra 'datum' shows undef value");
+ok( ! $object->exists_extra('datum'), "getting 'datum' value doesn't autoviv");
+
+$object->set_extra(datum => 10);
+
+ok($object->exists_extra('datum'), "extra 'datum' exists now");
+is($object->get_extra('datum'), 10, "extra/datum has the value we supplied");
+
+ok( ! $object->exists_misc('datum'), "there exists no misc 'datum' yet");
+is($object->get_misc('datum'), undef, "misc/datum has the value we supplied");
+
+$object->set_misc(datum => 20);
+
+ok($object->exists_misc('datum'), "there now exists misc 'datum'");
+is($object->get_misc('datum'), 20, "misc/datum has the value we supplied");
+
+is($object->get_extra('datum'), 10, "extra/datum has the value we supplied");
+
+$object->delete_extra('datum');
+
+ok( ! $object->exists_extra('datum'), "there exists no extra 'datum' again");
+is($object->get_extra('datum'), undef, "extra 'datum' shows undef value");
+
+is_deeply(
+  [ $object->get_all_misc_names ],
+  [ qw(datum) ],
+  "get_all_misc_names gets the one name that it should",
+);
+
+is_deeply(
+  [ $object->get_all_misc ],
+  [ datum => 20 ],
+  "get_all_misc gets the one pair that it should",
+);
+
+is_deeply(
+  [ $object->get_all_detailed_misc ],
+  [ datum => { value => 20 } ],
+  "get_all_detailed_misc gets the one pair that it should",
+);
